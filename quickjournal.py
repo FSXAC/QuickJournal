00374923b1da8da1c3d2e31960efe9b401133316
Muchen He <i@muchen.ca>
2021-09-29 08:33:54 -0700

    Added text line breaks

--- quickjournal.py
+++ quickjournal.py
@@ -24,8 +24,11 @@ ASCII_MIN = 32
 ASCII_MAX = 127
 
 # parameters
-MAX_CHARS = 280
+MAX_CHARS = 140
+PADDING = 1
 TITLE = '😋 QuickJournal'
+CURSOR = '\u258e'
+BREAK_SEPS = ' '
 
 def writeEntry(txt):
     home = str(Path.home())
@@ -35,9 +38,78 @@ def writeEntry(txt):
 
     with open(os.path.join(homepath, f'{date}.md'), 'a') as f:
         f.write(f'\n`{time}`\n\n')
-        f.write(txt.replace('\n', '\n\n'))
+        f.write(txt)
         f.write('---\n')
 
+def findBreakIndex(txt, max_width):
+    """
+    Given a long string, and maximimum width,
+    find the appropriate line break index(es)
+
+    Accepted separators include spaces and punctuations
+    """
+
+    if len(txt) <= max_width:
+        return []
+
+    part_txt = txt[:max_width]
+    txt_mask = ''.join(['0' if c in BREAK_SEPS else '1' for c in part_txt])
+    index = max_width - 1
+    while index > 0:
+        if txt_mask[index] == '0' and txt_mask[index - 1] == '1':
+            break
+        else:
+            index -= 1
+
+    remaining_txt = txt[index + 1:]
+    
+    return [index] + findBreakIndex(remaining_txt, max_width)
+
+
+def drawText(screen, screen_width, txt):
+    """Handles rendering of the main text, including line and word breaks"""
+
+    need_refresh = False
+
+    # How many characters can we fit inside a line (-1 for border, -1 for cursor)
+    max_line_width = screen_width - (2 * PADDING) - 3
+
+    lines = txt.split('\n')
+    break_offset = 0
+
+    # Cursor position
+    cursor_x = 0
+
+    for line in lines:
+
+        # Not suppose to be here, shoudl be per line txt
+        breaks = findBreakIndex(line, max_line_width)
+
+        if breaks:
+            # Display the first line
+            segment = line[:breaks[0]]
+            screen.addstr(1 + PADDING + break_offset, 1 + PADDING, segment)
+
+            for i in range(len(breaks)):
+                break_offset += 1
+                line = line[breaks[i] + 1:]
+
+                if (i == len(breaks) - 1):
+                    segment = line
+                else:
+                    segment = line[:breaks[i + 1]]
+                
+                screen.addstr(1 + PADDING + break_offset, 1 + PADDING, segment)
+                cursor_x = len(segment)
+        else:
+            screen.addstr(1 + PADDING + break_offset, 1 + PADDING, line)
+            cursor_x = len(line)
+
+        break_offset += 1
+    
+    screen.addch( PADDING + break_offset, cursor_x + 2, '\u258e')
+    
+    return need_refresh
 
 def main(screen):
 
@@ -57,23 +129,25 @@ def main(screen):
 
     while not done:
         screen.refresh()
+        need_refresh = False
 
+        # Draw borders
         vert_offset = txt_entry.count('\n')
-        height = (MAX_CHARS // width) + 1 + vert_offset
-        rectangle(screen, 0, 0, height, width - 1)
+        rect_height = (MAX_CHARS // (width - 2 * PADDING - 2)) + 2 * PADDING + vert_offset
+        rectangle(screen, 0, 0, rect_height, width - 1)
 
+        # Draw title over borders
         screen.addstr(0, 1, f'[{TITLE}]')
 
         # Draw entry text
-        for i, txt in enumerate(txt_entry.split('\n')):
-            screen.addstr(1 + i, 1, txt)
+        need_refresh |= drawText(screen, width, txt_entry)
 
-        current_line_length = len(txt_entry.split('\n')[-1])
-        screen.addch(1 + vert_offset, current_line_length + 1, '\u258e')
+        # current_line_length = len(txt_entry.split('\n')[-1])
+        # screen.addch(1 + vert_offset, current_line_length + 1, '\u258e')
 
         # text limit
-        txt_limit = f'{len(txt_entry)}/{MAX_CHARS}'
-        screen.addstr(height, 1, txt_limit)
+        txt_limit = f'[{len(txt_entry)}/{MAX_CHARS}]'
+        screen.addstr(rect_height, 1, txt_limit)
 
         # Handle keyboard stuff
         key = screen.getch()
@@ -90,14 +164,14 @@ def main(screen):
         
         elif key == RETURN:
             # new line
-            txt_entry += '\n'
-            screen.clear()
+            txt_entry += '\n\n'
+            need_refresh = True
         
         elif key == BACKSPACE:
             # Remove character from text entry
             if txt_entry:
                 txt_entry = txt_entry[:-1]
-                screen.clear()
+                need_refresh = True
 
         elif key == CMD_BACKSPACE:
             # Remove a whole word
@@ -108,17 +182,20 @@ def main(screen):
             else:
                 txt_entry = txt_entry[:space_index]
             
-            screen.clear()
+            need_refresh = True
 
         elif key == curses.KEY_RESIZE:
             height, width = screen.getmaxyx()
-            screen.clear()
+            need_refresh = True
 
         elif key == SEND:
             if txt_entry:
                 writeEntry(txt_entry)
                 txt_entry = ''
-                screen.clear()
+                need_refresh = True
+
+        if need_refresh:
+            screen.clear()
     
 
 if __name__ == '__main__':
